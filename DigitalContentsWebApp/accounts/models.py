d30086ee76c060dc568b3fc8e4aa35c3f322cc38
Vyadavgit <Vyadavgit@users.noreply.github.com>
2020-09-18 22:47:49 -0500

    I am pushing it only for testing

--- DigitalContentsWebApp/accounts/models.py
+++ DigitalContentsWebApp/accounts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+
 # Create your models here.
+class Subscriber(models.Models):
+    GENDER_CHOICES = [('Male', 'Male'), ('Female', 'Female'), ('Other', 'Other')]
+    first_name = models.CharField(max_length=250, null=True)
+    last_name = models.CharField(max_length=250, null=True)
+    email = models.CharField(max_length=250, null=True)
+    gender = models.CharField(max_length=25, choices=GENDER_CHOICES, blank=True)
+
+    def __str__(self):
+        if self.first_Name and self.last_Name:
+            identity = (self.first_Name + " " + self.last_Name)
+        else:
+            identity = str(self.id)
+        return identity
--- DigitalContentsWebApp/accounts/views.py
+++ DigitalContentsWebApp/accounts/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def homepage(request):
+    # # print("works well------------------------")
+    # return render(request, 'accounts/dashboard.html')
+    return HttpResponse('yay, It works!')
+
